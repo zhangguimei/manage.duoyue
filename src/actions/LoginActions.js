f5c1d7074113a510c667f4caf8ff930b5c295d30
Susan Su <sujiehua@duoyue.me>
2016-06-28 20:41:16 +0800

    fix conflict

--- src/actions/LoginActions.js
+++ src/actions/LoginActions.js
@@ -1,7 +1,58 @@
 'use strict';
-import {LOG_IN, LOG_OUT, LOGGED_IN} from '../constants/constants';
+import { push } from 'react-router-redux';
+import {LOG_IN, LOG_OUT, LOGGED_IN, GET_USERINFO} from '../constants/constants';
+import {parseJson} from 'UtilsFolder/getDataInfo';
 import auth from '../api/auth';
 import { getBookType } from '../api/axiosServices';
+import usercenterapi from 'APIFolder/UserCenter';
+import {showMsg} from './commonActions';
+
+export const loginByAccount = (loginInfo, callback) => {
+  return (dispatch)  => {
+    return usercenterapi.login(loginInfo)
+        .then(response => ({json: response.data, status: response.statusText})) //所有axios返回值都有data和statusText, 在data里面真正后台返回来的数据
+        .then(({json,status}) => {
+          if(status !== 'OK'){
+            return dispatch(showMsg(json.message || '登录失败'));
+          }
+
+          let returnData = json.data;
+          //得到token,并存储
+          auth.setToken(returnData.token);
+
+          dispatch(loginSuccess(returnData));
+         // fetchTreedata(returnData.userName);
+          console.log("操作员是 ", returnData.userName, returnData.permissions);
+
+          callback && callback();
+        }).catch(err=>{
+          callback && callback(err.data, err.status);
+        })
+  }
+}
+
+export const loginByToken = (callback) => {
+  return (dispatch)  => {
+    return usercenterapi.getInfoByToken()
+        .then(response => ({json: response.data, status: response.statusText})) //所有axios返回值都有data和statusText, 在data里面真正后台返回来的数据
+        .then(({json,status}) => {
+          if(status !== 'OK'){
+            return dispatch(showMsg(json.message || '登录失败'));
+          }
+
+          let returnData = json.data;
+
+          dispatch(loginSuccess(returnData));
+          // fetchTreedata(returnData.userName);
+         // console.log("操作员是 ", returnData.userName, returnData.permissions);
+
+          callback && callback();
+        }).catch(err=>{
+          auth.logout();
+          callback && callback(err.data, err.status);
+        })
+  }
+}
 
 export const fetchTreedata = (username) => {
   //getBookType();
@@ -10,7 +61,7 @@ export const fetchTreedata = (username) => {
 
     case 'admin':
       tree = require("../assets/MockData/tree_data.json");
-        //console.log(tree.menu);
+        console.log(tree.menu);
 
       const dataMap = tree.menu.reduce(function (map, category) {
         category.itemsMap = category.data.reduce(function (itemsMap, item) {
@@ -29,7 +80,7 @@ export const fetchTreedata = (username) => {
         return map;
       }, {});
 
-      //console.log("susan" ,dataMap);
+      console.log("susan" ,dataMap);
 
       break;
     case 'admin2':
@@ -38,31 +89,43 @@ export const fetchTreedata = (username) => {
     default:
       tree = require("../assets/MockData/tree_data.json");
   }
+  parseJson(tree.menu,'');
+  parseJson(tree.fast,'');
   //console.log(tree);
   return dispatch => {
     return dispatch(loggedIn(username, tree));
   }
 }
 
-const loggedIn = (username, tree) => {
+//退出登录
+export const doLogOut = () => {
+  return dispatch => {
+    // usercenterapi.loginOut();  //登出API还有问题??
+    auth.logout();
+    return dispatch(logOut());
+  }
+}
+
+const loggedIn = (userName, tree) => {
   return {
     type: LOGGED_IN,
-    username,
+    userName,
     tree
   }
 }
 
-export const logIn = (username, pwd)=> {
-  auth.login()
+//通过账号登录
+export const loginSuccess = (login)=> {
   return {
     type: LOG_IN,
-    username
+    login
   }
 }
 
-export const logOut = (username)=> {
+export const logOut = ()=> {
   return {
-    type: LOG_OUT,
-    username
+    type: LOG_OUT
   }
 }
+
+
