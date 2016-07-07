d1437616e70c6858f529c3e82f7fbe2fbc837223
Pancheng <772045739@qq.com>
2016-07-07 10:03:24 +0800

    optimize accesspath and sidebar

--- src/routes/ManagePage/components/Sidebar/SidebarItem.js
+++ src/routes/ManagePage/components/Sidebar/SidebarItem.js
@@ -2,6 +2,7 @@
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
 import SidebarItemList from './SidebarItemList';
+import {regPath} from 'UtilsFolder/getDataInfo';
 
 class SidebarItem extends React.Component {
   constructor(props) {
@@ -19,7 +20,7 @@ class SidebarItem extends React.Component {
   }
 
   render() {
-    const {menuData:{permissionName, children, icon_min, accessPath}, path, changeRoute, parent} = this.props,
+    const {menuData:{permissionName, children, icon_min, accessPath}, path, changeSubData} = this.props,
       {open} = this.state, ITEM_HEIGHT = 30;
     return (
       <div className="SidebarItem item">
@@ -31,7 +32,7 @@ class SidebarItem extends React.Component {
                 <i className={open ? 'ic ic-move ic2' :'ic ic-add ic2'}/>
               </a>
               :
-              <Link to={accessPath} className={path.indexOf(accessPath) > -1 ? 'cur' : null} onClick={() => changeRoute(parent.split(".").slice(0,-1))}>
+              <Link to={regPath(accessPath)} className={path.indexOf(accessPath) > -1 ? 'cur' : null}>
                 <i className={`ic ${icon_min} ic1`}/>
                 {permissionName}
               </Link>
@@ -42,7 +43,7 @@ class SidebarItem extends React.Component {
               {
                 children.map((item, index) => {
                   return (
-                    <SidebarItemList key={index}  menuData={item} parent={`${parent}${index}`} changeRoute={changeRoute} path={path}/>
+                    <SidebarItemList key={index}  menuData={item} changeSubData={changeSubData} path={path}/>
                   );
                 })
               }
